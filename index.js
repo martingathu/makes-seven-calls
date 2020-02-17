a41cdf8dce948d85aa0a4c9df28d6956915c831b
martingathu <martin5gathu@gmail.com>
2020-02-17 18:05:19 +0300

    create a function for loops

---
+++ index.js
@@ -0,0 +1,10 @@
+function myFunction(n){
+    var triangle="";
+      for(let i=1; i<=n; i++)
+      {
+      triangle += '#'; 
+        console.log(triangle);
+      }
+    }
+    myFunction(7)
+    
